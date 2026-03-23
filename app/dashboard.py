53bead92a30872c035483fbcc398c5b2423ae5ba
LuizMoassab8801 <guigiraud1@hotmail.com>
2026-03-23 10:47:29 -0300

    Update comment on renewable energy resources
    Clarify note about renewable energy resources in Egypt.

--- app/dashboard.py
+++ app/dashboard.py
@@ -7,4 +7,4 @@ import numpy
 ### Utilizar do banco de dados tratados
 ### Focar nos países com maior aumento ao longo do tempo
 ### Evidenciar grandes proporções de energia limpa em países
-### Talvez implementar outro csv com disponibilidade de recursos para energia reutilizavel em países que não a usam primariamente
+### Talvez implementar outro csv com disponibilidade de recursos para energia reutilizavel em países que não a usam primariamente(Egito tem segundo maior rio do mundo e não usa energia hidroelétrica)
