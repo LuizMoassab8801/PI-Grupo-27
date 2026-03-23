0ce5a05bd9de79f1001b631ea738880d62c642b9
LuizMoassab8801 <guigiraud1@hotmail.com>
2026-03-23 10:45:25 -0300

    Update dashboard.py

--- app/dashboard.py
+++ app/dashboard.py
@@ -1,3 +1,10 @@
 import streamlit as st
 import panda as pd
 import numpy
+
+
+### Implementar dashboard usando streamlit(perguntar se necessário usar streamlit)
+### Utilizar do banco de dados tratados
+### Focar nos países com maior aumento ao longo do tempo
+### Evidenciar grandes proporções de energia limpa em países
+### Talvez implementar outro csv com disponibilidade de recursos para energia reutilizavel em países que não a usam primariamente
